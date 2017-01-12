a9344eb139a6dbdffcaea043f181b177114047b1
Noel Cothren <noelcothren@gmail.com>
2017-01-11 18:22:49 -0700

    set up the basic HTML tags for future content

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Pinterest New Pin</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<h2>Adding a New Pin on Pinterest.com</h2>
+		<h3>User Persona</h3>
+		<p>This is a user's details</p>
+		<h3>Use Case</h3>
+		<p>This is details of their use - i.e. pinning a new cool pic to reference later</p>
+		<h3>Interaction Map</h3>
+		<p>This is where I outline how the interaction goes down.</p>
+	</body>
+</html>
