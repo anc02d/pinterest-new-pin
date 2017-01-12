e97394f2b9e80085dd8964f640b4930ed841629a
Noel Cothren <noelcothren@gmail.com>
2017-01-11 18:55:21 -0700

    added content for a user persona

--- index.php
+++ index.php
@@ -8,17 +8,24 @@
 		<h1>Data Design Project</h1>
 		<h2>Adding a New Pin on Pinterest.com</h2>
 		<h3>User Persona</h3>
-		<p>This is a user's details</p>
+		<ul>
+			<li>Name: Ashley Smith</li>
+			<li>Age: 24</li>
+			<li>Occupation: college student living off student loans and daddy's money </li>
+			<li>Technology: iPhone7 and a Mac Book</li>
+			<li>Needs: to find a hairstyle for tomorrow's big date </li>
+			<li>Goals: to build a collection of inspiration and ideas for future reference </li>
+		</ul>
 		<h3>Use Case</h3>
 		<p>This is details of their use - i.e. pinning a new cool pic to reference later</p>
 		<h3>Interaction Flow</h3>
-		<p>How the interaction goes down.</p>
+		<h4>How the interaction goes down.</h4>
 			<ol>
 			<li>User: Opens Pinterest app on phone</li>
 			<li>Site: Auto logs user in based on saved data and presents default pin feed screen</li>
 			<li>User: Scrolls down to look at feed</li>
 			<li>Site: Loads more pins as user scrolls down</li>
-			<li>User: Sees pin she likes and wants to save to her boards, taps and holds on picture</li>
+			<li>User: Sees a hairstyle pin she likes and wants to save to her boards, taps and holds on picture</li>
 			<li>Site: Pops up 3 icons - save, like, send</li>
 			<li>User: Drags finger onto "save" icon</li>
 			<li>Site: Opens choose board screen with user's boards</li>
