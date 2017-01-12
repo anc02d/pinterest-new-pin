28aab54a811435e177af9547f4d688a356e6683a
Noel Cothren <noelcothren@gmail.com>
2017-01-12 08:31:38 -0700

    strong tags for emphasis, justify p, fewer paragraphs

--- index.php
+++ index.php
@@ -10,18 +10,16 @@
 		<h2>Adding a New Pin on Pinterest.com</h2>
 		<h3>User Persona</h3>
 		<ul>
-			<li>Name: Ashley Smith</li>
-			<li>Age: 24</li>
-			<li>Occupation: college student living off student loans and daddy's money </li>
-			<li>Technology: iPhone7 and a Mac Book</li>
-			<li>Needs: to find a hairstyle for tomorrow's big date </li>
-			<li>Goals: impress her social group with her creativity and to build a collection of inspiration and ideas for future reference </li>
+			<li><strong>Name:</strong>Ashley Smith</li>
+			<li><strong>Age:</strong> 24</li>
+			<li><strong>Occupation:</strong> college student living off student loans and daddy's money </li>
+			<li><strong>Technology:</strong> iPhone7 and a Mac Book</li>
+			<li><strong>Needs:</strong> to find a hairstyle for tomorrow's big date </li>
+			<li><strong>Goals:</strong> impress her social group with her creativity and to build a collection of inspiration and ideas for future reference </li>
 		</ul>
 		<h3>Use Case</h3>
-		<p>Ashley is done with class for the day and looking forward to her big date with Brad tomorrow. She wants to impress him and knows that a new hairstyle will do the trick. So, she turns to Pinterest for some inspiration.</p>
-		<p>After opening the site on her iPhone app, she starts scrolling through her feed. She sees a hairstyle she likes and decides to pin it to her "Hair" board so it will be accessible tomorrow when she's getting ready.</p>
-		<p>She simply taps and holds on the picture she likes, and slides her finger to the "save" icon. Then, she is prompted to edit the description, and she chooses her "Hair" board as the location of her new pin.</p>
-		<p>Once she taps the board she wants, her pin feed reappears along with a message that her pin was saved. She then continues browsing for the next 6 hours instead of finishing her homework.</p>
+		<p>Ashley is done with class for the day and looking forward to her big date with Brad tomorrow. She wants to impress him and knows that a new hairstyle will do the trick. So, she turns to Pinterest for some inspiration. After opening the site on her iPhone app, she starts scrolling through her feed. She sees a hairstyle she likes and decides to pin it to her "Hair" board so it will be accessible tomorrow when she's getting ready.</p>
+		<p>She simply taps and holds on the picture she likes, and slides her finger to the "save" icon. Then, she is prompted to edit the description, and she chooses her board called "Hair" as the location of her new pin. Once she taps the board she wants, her pin feed reappears along with a message that her pin was saved. She then continues browsing for the next 6 hours instead of finishing her homework.</p>
 		<h3>Interaction Flow</h3>
 			<ol>
 			<li class="user">User: Opens Pinterest app on phone</li>
