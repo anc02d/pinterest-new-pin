64f4c524bb2ddfde882f01885308e0e430ddcc02
Noel Cothren <noelcothren@gmail.com>
2017-01-12 09:49:01 -0700

    content and tabbing edit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			<li><strong>Name:</strong>Ashley Smith</li>
 			<li><strong>Age:</strong> 24</li>
 			<li><strong>Occupation:</strong> college student living off student loans and daddy's money </li>
-			<li><strong>Technology:</strong> iPhone7 and a Mac Book</li>
+			<li><strong>Technology:</strong> iPhone7 and a Mac Book running OS 10 Sierra</li>
 			<li><strong>Needs:</strong> to find a hairstyle for tomorrow's big date </li>
 			<li><strong>Goals:</strong> impress her social group with her creativity and to build a collection of inspiration and ideas for future reference </li>
 		</ul>
@@ -21,7 +21,7 @@
 		<p>Ashley is done with class for the day and looking forward to her big date with Brad tomorrow. She wants to impress him and knows that a new hairstyle will do the trick. So, she turns to Pinterest for some inspiration. After opening the site on her iPhone app, she starts scrolling through her feed. She sees a hairstyle she likes and decides to pin it to her "Hair" board so it will be accessible tomorrow when she's getting ready.</p>
 		<p>She simply taps and holds on the picture she likes, and slides her finger to the "save" icon. Then, she is prompted to edit the description, and she chooses her board called "Hair" as the location of her new pin. Once she taps the board she wants, her pin feed reappears along with a message that her pin was saved. She then continues browsing for the next 6 hours instead of finishing her homework.</p>
 		<h3>Interaction Flow</h3>
-			<ol>
+		<ol>
 			<li class="user">User: Opens Pinterest app on phone</li>
 			<li class="site">Site: Auto logs user in based on saved data and presents default pin feed screen</li>
 			<li class="user">User: Scrolls down to look at feed</li>
@@ -32,7 +32,7 @@
 			<li class="site">Site: Opens choose board screen with user's boards</li>
 			<li class="user">User: Enters a description, chooses a board to save to and taps on it</li>
 			<li class="site">Site: Saves to database, Goes back to pin feed and gives a pop-up "saved" message at bottom</li>
-			</ol>
+		</ol>
 		<h3>Conceptual Model</h3>
 		<ul>
 			<li>One user can upload many pins</li>
