c6569f3dfed001b2f82b5890d5a66601aac4f0ec
Noel Cothren <noelcothren@gmail.com>
2017-01-11 18:39:38 -0700

    added content in ordered list for Interaction Flow

--- index.php
+++ index.php
@@ -11,7 +11,19 @@
 		<p>This is a user's details</p>
 		<h3>Use Case</h3>
 		<p>This is details of their use - i.e. pinning a new cool pic to reference later</p>
-		<h3>Interaction Map</h3>
-		<p>This is where I outline how the interaction goes down.</p>
+		<h3>Interaction Flow</h3>
+		<p>How the interaction goes down.</p>
+			<ol>
+			<li>User: Opens Pinterest app on phone</li>
+			<li>Site: Auto logs user in based on saved data and presents default pin feed screen</li>
+			<li>User: Scrolls down to look at feed</li>
+			<li>Site: Loads more pins as user scrolls down</li>
+			<li>User: Sees pin she likes and wants to save to her boards, taps and holds on picture</li>
+			<li>Site: Pops up 3 icons - save, like, send</li>
+			<li>User: Drags finger onto "save" icon</li>
+			<li>Site: Opens choose board screen with user's boards</li>
+			<li>User: Enters a description, chooses a board to save to and taps on it</li>
+			<li>Site: Saves to database, Goes back to pin feed and gives a pop-up "saved" message at bottom</li>
+			</ol>
 	</body>
 </html>
