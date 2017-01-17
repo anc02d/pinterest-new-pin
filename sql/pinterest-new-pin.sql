9f8320400ebfffde523eda00024353f8222be799
Noel Cothren <noelcothren@gmail.com>
2017-01-17 15:22:27 -0700

    updated svg file and some changes to SQL variable names

--- sql/pinterest-new-pin.sql
+++ sql/pinterest-new-pin.sql
@@ -1,17 +1,18 @@
--- this is a DDL
-DROP TABLE IF EXISTS 'board pin';
+
+DROP TABLE IF EXISTS 'boardPin';
 DROP TABLE IF EXISTS pin;
 DROP TABLE IF EXISTS board;
 DROP TABLE IF EXISTS profile;
 -- creating the tables
 CREATE TABLE profile (
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	profileName VARCHAR(32),
+	profileAtHandle VARCHAR(32),
 	profileDescription VARCHAR(128),
 	profilePasswordHash CHAR(128),
 	profileSalt CHAR(64),
 	UNIQUE(profileId),
-	PRIMARY KEY(profileId),
+	UNIQUE(profileAtHandle),
+	PRIMARY KEY(profileId)
 );
 
 CREATE TABLE board (
@@ -20,21 +21,24 @@ CREATE TABLE board (
 	boardName VARCHAR(32),
 	INDEX(boardProfileId),
 	FOREIGN KEY(boardProfileId) REFERENCES profile(profileId),
-	PRIMARY KEY(boardId),
+	PRIMARY KEY(boardId)
 );
 
 CREATE TABLE pin (
 	pinId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	pinTitle VARCHAR(64),
 	pinDescription VARCHAR(140),
-	pinImage VARCHAR (??),
+	pinImage VARCHAR (256),
 	pinUrl VARCHAR (256),
 	UNIQUE(pinId),
+	UNIQUE(pinUrl),
 	PRIMARY KEY(pinId)
 );
 
-CREATE TABLE 'board pin' (
+CREATE TABLE 'boardPin' (
 	boardPinPinId INT UNSIGNED NOT NULL,
 	boardPinBoardId INT UNSIGNED NOT NULL,
-	FOREIGN KEY(boardPinPinId) REFERENCES pin(pinId)
-)
+	FOREIGN KEY(boardPinBoardId) REFERENCES board(boardId),
+	FOREIGN KEY(boardPinPinId) REFERENCES pin(pinId),
+	PRIMARY KEY(boardPinBoardId, boardPinPinId)
+);
